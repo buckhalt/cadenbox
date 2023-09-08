e0819e6b3914dab02815415edb2cf305f497eeed
Caden Buckhalt <halaeb@gmail.com>
2023-09-07 19:09:32 -0700

    useRouter instead of reroute for game start

--- app/game/[code]/page.tsx
+++ app/game/[code]/page.tsx
@@ -21,11 +21,13 @@ export default function Game({ params }: { params: { code: string } }) {
     <main className="flex min-h-screen flex-col items-center p-24">
       <Card>
         <CardHeader>
-          <CardTitle>Welcome to Cadenbox</CardTitle>
-          <CardDescription>Game code: {code}</CardDescription>
+          <CardTitle>Game code: {code}</CardTitle>
+          <CardDescription>
+            Visit localhost:3000/play to join using the game code above
+          </CardDescription>
         </CardHeader>
         <CardContent>
-          {game?.players ? (
+          {game?.players.length && game.players.length > 0 ? (
             <div className="flex flex-col items-center">
               {game.players.map((player) => (
                 <div key={player} className="text-center">
--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,22 @@
 import { useMutation, useQuery } from "convex/react";
 import { api } from "~/convex/_generated/api";
 import { faker } from "@faker-js/faker";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { Button } from "~/components/ui/button";
 
 export default function Home() {
   const createGame = useMutation(api.games.createGame);
-  const games = useQuery(api.games.getGames);
+  const router = useRouter();
 
   const newGame = () => {
     const code = faker.word.noun(4).toUpperCase();
     createGame({ code });
-    redirect(`/game/${code}`);
+    router.push(`/game/${code}`);
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      {games?.map((game) => (
-        <div key={game.code}>{game.code}</div>
-      ))}
-      <Button variant="outline" onClick={newGame}>
-        New Game
-      </Button>
+      <Button onClick={newGame}>New Game</Button>
     </main>
   );
 }
